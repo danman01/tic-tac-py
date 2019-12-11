de7a92b83ec63b0a206cdbe5f9c8900256dc4250
Danny Kirschner <rkirschner1377@gmail.com>
2019-12-11 08:59:26 -0500

    Game logic complete
    Tests nearly covering all game functions
    
    import game or import test to check it out

--- game.py
+++ game.py
@@ -1,82 +1,164 @@
 # import 'math'
+# for pretty printing a matrix...
+import numpy as np
 
-# gameBoard = [
-#     [0,1,2],
-#     [3,4,5],
-#     [6,7,8]
-# ]
+CONSTANTS = {}
+START_INSTRUCTIONS = 'run game.new_game() to start'
 
-PLAYERS = ['x', 'y']
+print(START_INSTRUCTIONS)
 
-current_player = PLAYERS[0]
-
-winning_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-
-# hold game moves
-BOARD = [
-    ['','',''],
-    ['','',''],
-    ['','','']
-]
-
+# no tests!
 def start_game(board, current, winning_combos):
     # player_moves = ['','','','','','','','','','']
-    while(len(free_spaces(board)) > 0):
+    turn = 0
+    while(turn < 9):
+        # make sure to set empty strings for non-filled spaces, otherwise out of range error
         take_turn(board, current)
-        #  - create player index
-        #  - check_for_winner
-        #  - switch player
-        # for i, v in enumerate(board):
-        #     if (v == current_player):
-        #         player_moves[i] = i # make sure to set empty strings for non-filled spaces, otherwise out of range error
-        print(check_for_winner(board, winning_combos, current))
-        current_player = switch_player(current)
+        turn += 1
+        winner, sequence = check_for_winner(board, winning_combos, current)
 
+        if not winner:
+            if not check_for_draw(board, winning_combos):
+                # keep playing
+                current = switch_player(current)
+            else:
+                print(f"Draw! {CONSTANTS['restart_instructions']}")
+                break
+        else:
+            # winner!
+            print(f"{CONSTANTS['restart_instructions']}")
+            break
+
+# no tests!
 def take_turn(board, current):
-    '''AI taking a turn'''
+    '''taking a turn'''
     free_space_array = free_spaces(board)
     #i = math.ran(len(free_space_array))
-    i = (2,1)
+    res_string = input(f"{current}, what space would you like? spaces available: {free_space_array}")
+    x,y = res_string.split(",")
     # beep boop beep
-    board[i[0]][i[1]] = current
+    board[int(x)][int(y)] = current
+    # or use insert?
+    print(f'board is now\n {np.matrix(board)}')
+    # puts(f'board is now {board}')
     # each turn:
     #  - if open space...
     #  - play in open space x
     #  - play in open space o
 
 def check_for_winner(board, winningCombos, player):
+    ''' check board for winner. returns True or False, and winning sequence if there is one '''
+    # use count to see if >= 3 of player?
+
     # if board contains any winningCombos ?
     # need current_player_board: 2d array of where player tokens are
-    winner = ''
+    winner = False
+    sequence = None
+    current_player_board = get_player_positions(board, player)
 
-    for index, x in enumerate(winningCombos):
-        if (board.include(x)):
-            print('winner!')
-    # for i, x in enumerate(winningCombos):
-    #     if (playerIndex[x[0]] == x[0] and playerIndex[x[1] == x[1]] and playerIndex[x[2] == x[2]]):
-    #         winner = player
-    #         print 'winner!'
-    #         break
-    return winner
+    for combo in winningCombos:
+        # is [0,1,2] in array of indexes of 'X'? [0,1,2,4,8]
+        if(combo[0] in current_player_board and combo[1] in current_player_board and combo[2] in current_player_board):
+            winner = True
+        else:
+            winner = False
+        if(winner):
+            sequence = combo
+            print(f'Winner! {player} won with {sequence}: {board}')#.format(player=player, board=board))
+            break
+    return [winner, sequence]
+
+# attempting to have this functional...
+# ...but it could easily be refactored and slimmed down if we know where we are in the game flow
+# ...based on no winner / turn count
+def check_for_draw(board, winningCombos):
+    new_board = get_1d_board(board)
+    # remove empty spaces
+    while(new_board.count('') > 0):
+        new_board.remove('')
+
+    if(len(new_board) == 9):
+        # look at every combo...is there a winner?
+        for combo in winningCombos:
+            if(new_board[combo[0]] == new_board[combo[1]] == new_board[combo[2]]):
+                # winner!
+                # TODO: check for winner here, instead of calling multiple funcs?
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def get_player_positions(board, player):
+    '''creates and returns 1d board of player indexes'''
+    new_board = [] # 1d board
+    for r, row in enumerate(board):
+        for s, space in enumerate(row):
+            if space == player:
+                index = 0
+                if r == 0:
+                    index += 0
+                elif r == 1:
+                    index += 3
+                elif r == 2:
+                    index += 6
+
+                new_board.append(index + s)
+    return new_board
+
+def get_1d_board(board):
+    '''creates and returns 1d board of player tokens at indexes'''
+    new_board = [] # 1d board
+    for r, row in enumerate(board):
+        for s, space in enumerate(row):
+            index = 0
+            if r == 0:
+                index += 0
+            elif r == 1:
+                index += 3
+            elif r == 2:
+                index += 6
+
+            new_board.insert(index + s, space)
+    return new_board
 
 def free_spaces(board):
     free = []
     # loop through each spot on the board array
     # return an array of the free coordinates to choose from
-    for r in range(0, 2): #(r = 0; r < 3; r++):
-        for c in range(0, 2):#(c = 0; c < 3; c++):
+    for r in range(0, 3): #(r = 0; r < 3; r++):
+        for c in range(0, 3):#(c = 0; c < 3; c++):
             if (board[r][c] == ''):
                 free.append((r, c))
     return free
 
 def switch_player(current):
-    if(current == PLAYERS[0]):
-        to_return = PLAYERS[1]
+    if(current == CONSTANTS['players'][0]):
+        return CONSTANTS['players'][1]
     else:
-        to_return = PLAYERS[0]
-    return to_return
+        return CONSTANTS['players'][0]
 
+def reset_game():
+    _setup()
+    return 'reset...'
 
-################
-### TESTS ######
-################
+def _setup():
+    CONSTANTS['players'] = [input("Player 1:"), input("Player 2:")]
+    CONSTANTS['current_player'] = CONSTANTS['players'][0]
+    CONSTANTS['winning_combos'] = winning_combos()
+    CONSTANTS['restart_instructions'] = START_INSTRUCTIONS.replace('start','restart')
+    CONSTANTS['board'] = [
+        ['','',''],
+        ['','',''],
+        ['','','']
+    ]
+
+def play():
+    start_game(CONSTANTS['board'], CONSTANTS['current_player'], CONSTANTS['winning_combos'])
+
+def new_game():
+    reset_game()
+    play()
+
+def winning_combos():
+    return [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
--- test.py
+++ test.py
@@ -1,48 +1,171 @@
+from flask import Flask
 from inspect import currentframe
-from game import BOARD, free_spaces
+from game import CONSTANTS as GAME_CONSTANTS, winning_combos, free_spaces, switch_player, get_player_positions, check_for_winner, check_for_draw, get_1d_board, reset_game
 
+app = Flask(__name__)
+
+reset_game()
 # Unit tests #
-TEST_COUNTER = 0
-ERRORS = {}
+CONSTANTS = {}
+print('run_unit_tests() to run tests...pass debug=true to turn on debugger when error')
+def test_setup(debug):
+    CONSTANTS['test_results']=[]
+    CONSTANTS['errors'] = {}
+    CONSTANTS['new_game_board'] = [
+        ['','',''],
+        ['','',''],
+        ['','','']
+    ]
+    CONSTANTS['completed_game'] = [
+        ['X','X','O'],
+        ['X','X','O'],
+        ['O','O','X']
+    ]
+    CONSTANTS['draw_game'] = [
+        ['O','X','O'],
+        ['X','X','O'],
+        ['O','O','X']
+    ]
+    CONSTANTS['incomplete_game'] = [
+        ['O','X','O'],
+        ['X','X',''],
+        ['O','','']
+    ]
+    if(debug):
+        CONSTANTS['debug'] = True
+    else:
+        CONSTANTS['debug'] = False
+
+    return 'setup complete'
 
 def add_errors(func, msg):
-    ERRORS[func] = msg
+    CONSTANTS['errors'][func] = msg
+
+def get_meth_name(meth_name):
+    return meth_name.f_code.co_name.replace('test_','')
+
+def expect_equality(meth, item1, item2):
+    print(f'testing {meth}.  .  .   .  ... .. ...')
+    if (item1 == item2):
+        CONSTANTS['test_results'].append(True)
+        print('passed!')
+    else:
+        CONSTANTS['test_results'].append(False)
+        msg = f'failed! item1 {item1} does not match {item2}'#.format(item1=item1, item2=item2)
+        add_errors(meth, msg)
+        if(CONSTANTS['debug'] == True):
+            # import pdb; pdb.set_trace()
+            breakpoint()
+
+def test_check_for_winner(board, winningCombos, player):
+    meth = get_meth_name(currentframe())
+    print(f'{meth} determine if there is a winner, and what winning sequence is')#.format(meth=meth))
+    winner, sequence = check_for_winner(board, winningCombos, player)
+    correct_sequence = [0,4,8]
+    expect_equality(meth, winner, True)
+    expect_equality(meth, sequence, correct_sequence)
+
+def test_check_for_draw(board, winningCombos):
+    meth = get_meth_name(currentframe())
+    print(f'{meth} determine if there is a draw')#.format(meth=meth))
+    result = check_for_draw(board, winningCombos)
+    expect_equality(meth, result, True)
+
+def test_check_for_no_draw(board, winningCombos):
+    meth = get_meth_name(currentframe())
+    print(f'{meth} determine if there is not a draw')#.format(meth=meth))
+    result = check_for_draw(board, winningCombos)
+    expect_equality(meth, result, False)
+
+def test_get_player_positions(board, player):
+    meth = get_meth_name(currentframe())
+    print(f'{meth} return indexes where player tokens are')#.format(meth=meth))
+    current_board = get_player_positions(board, player)
+    test_board_x_indexes = [0,1,3,4,8]
+    expect_equality(meth, current_board, test_board_x_indexes)
+
+def test_switch_player():
+    meth = get_meth_name(currentframe())
+    print(f'{meth} should switch CURRENT_PLAYER from x to o, or o to x')#.format(meth=meth))
+    player = 'X'
+    correct_new_player = 'O'
+    new_player = switch_player(player)
+    expect_equality(meth, new_player, correct_new_player)
 
 def test_free_spaces():
-    print('.free_spaces given empty board should return collection of all possible coords')
-    TEST_COUNTER + 1
+    meth = get_meth_name(currentframe())
+    print(f'{meth} given empty board should return collection of all possible coords')#.format(meth=meth))
     # setup known good values
     collection_of_all_coords = []
 
-    for i in range(0, 2):
-        for n in range(0, 2):
+    # buggy...just hardcode it!
+    for i in range(0, 3):
+        for n in range(0, 3):
             collection_of_all_coords.append((i, n))
             # [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
 
     # setup values from app
-    board1 = BOARD # copy? clone?
+    board1 = CONSTANTS['new_game_board'] # copy? clone?
     free = free_spaces(board1)
 
     # assertion
-    if(free == collection_of_all_coords):
-        print('pass')
-    else:
-        print('fail')
-        add_errors(currentframe().f_code.co_name,'failed! Board wasn\'t empty')
+    expect_equality(meth, free, collection_of_all_coords)
 
-    # output
-    # print(res)
+def test_get_1d_board(board):
+    meth = get_meth_name(currentframe())
+    print(f'{meth} get 1d board from gameboard')#.format(meth=meth))
+    new_board = get_1d_board(board)
+    # based on looking at passed in board...
+    correct_1d_board = ['X','X','O','X','X','O','O','O','X']
+    expect_equality(meth, new_board, correct_1d_board)
 
-def run_unit_tests():
-    print('\n\n***** Unit Tests ******\n\n')
+def test_reset_game():
+    meth = get_meth_name(currentframe())
+    print(f'{meth} resetting game and game constants')
+    board = GAME_CONSTANTS['board']
+    fresh_board = [
+        ['','',''],
+        ['','',''],
+        ['','','']
+    ]
+    reset_game()
+    free_spaces_array = free_spaces(board)
+    expect_equality(meth, get_1d_board(board), get_1d_board(fresh_board))
+    expect_equality(meth, len(free_spaces_array), 9)
+
+@app.route("/test")
+def run_unit_tests(debug = False):
+    html = '\n***** Unit Tests ******\n'
+    html += test_setup(debug)
+    # todo:
+    # test_start_game()
+    # test_take_turn()
     test_free_spaces()
+    test_switch_player()
+    test_get_player_positions(CONSTANTS['completed_game'], 'X')
+    test_check_for_winner(CONSTANTS['completed_game'], winning_combos(), 'X')
+    test_check_for_draw(CONSTANTS['draw_game'], winning_combos())
+    test_check_for_no_draw(CONSTANTS['incomplete_game'], winning_combos())
+    test_get_1d_board(CONSTANTS['completed_game'])
+    test_reset_game()
+
 
     # print errors:
-    for k, v in ERRORS.items():
-        print('error! {k}: {v}'.format(k=k, v=v))
+    html += "\n**** errors: **** \n"
+    for k, v in CONSTANTS['errors'].items():
+        html += f'error! {k}: {v}'#.format(k=k, v=v)
+
+    html += test_stats()
+    print(html)
+
 
 # test stats
 def test_stats():
-    print(TEST_COUNTER)
-    print('tests complete')
-# Integration tests #
+    str = '\n***** Unit Test Stats ******\n'
+    str += '{passed} passed. {failed} failed'.format(passed=CONSTANTS['test_results'].count(True), failed=CONSTANTS['test_results'].count(False))
+    str += 'tests complete'
+    return str
+
+# flask
+if __name__ == "__main__":
+    app.run()
